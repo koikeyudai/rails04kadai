8d6502ae6052474bb6a617a6edae3f1c8ae2e07a
koikeyudai <yuubass1007@gmail.com>
2019-08-28 00:58:50 +0000

    rails04kadainew

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
-validates :name, presence: true, length: {maximum: 15}
+  validates :name, presence: true, length: {maximum: 15}
+  
+  VALID_EMAIL_REGEX = /[A-Za-z0-9._+]+@[A-Za-z]+.[A-Za-z]/
+  validates :email, presence: true, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
+  
+  VALID_PASSWORD_REGEX = /\A(?=.?[a-z])(?=.?\d)[a-z\d]{8,32}\z/i
+  validates :password, presence: true, format: {with: VALID_PASSWORD_REGEX}
+  
+  has_secure_password
 
-VALID_EMAIL_REGEX = /\A[\w+-.]+@[a-z\d-.]+.[a-z]+.[A-Z]+\z/i
-validates :email, presence: true, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
-
-VALID_PASSWORD_REGEX = /\A(?=.?[a-z])(?=.?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,32}+\z/
-validates :password_digest, presence: true, uniqueness: true, format: {with: VALID_PASSWORD_REGEX}
-
-has_secure_password
 end
